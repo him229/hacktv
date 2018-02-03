40b57b1a14e1deef527abb70c484420c3d68ecb3
Muin Momin <momin.muin@gmail.com>
2018-02-03 09:11:11 -0600

    testing word cloud

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, render_template
 import json
+from wordcloud import WordCloud
+from time import sleep
+
 
 app = Flask(__name__)
 
+
+def show_wordcloud(text):
+    wordcloud = WordCloud(background_color="white").generate_from_text(text)
+    image = wordcloud.to_image()
+    image.show()
+    return image
+
+
+# junk = "ready to drink meail with chai soulent cafe chai natural and artificial flavors complete meal 20 percent daily nutrition 20 grams protein caffeine plus one dash theanine 14 fluid ounces"
+# show_wordcloud(junk)
+# sleep(2)
+# junk += "cat cat cat cat cat cat cat cat cat cat cat cat"
+# show_wordcloud(junk)
+
+
 @app.route('/msg', methods=['GET', 'POST'])
 def get_msg():
-    print (request.form['Body'])
+    msg = request.form['Body']
+    print(msg)
     return request.form['Body']
+
 
 @app.route('/')
 def index():
-    return "It's working"
+    return render_template("index.html")
+
 
 if __name__ == '__main__':
     app.run()
